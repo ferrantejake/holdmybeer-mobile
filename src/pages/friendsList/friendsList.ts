0ada23faaccfc35c4a4f339fc728fb4ef168db81
Brett Belcher <brettbelcher@Knights.ucf.edu>
2017-04-04 00:12:28 -0400

    Updated System to Ionic2

---
+++ src/pages/friendsList/friendsList.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-friendsList',
+  templateUrl: 'friendsList.html'
+})
+export class FriendsList {
+
+  constructor(public navCtrl: NavController) {
+    
+  }
+
+}
---
+++ src/pages/tasteProfile/tasteProfile.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-tasteProfile',
+  templateUrl: 'tasteProfile.html'
+})
+export class TasteProfile {
+
+  constructor(public navCtrl: NavController) {
+    
+  }
+
+}
