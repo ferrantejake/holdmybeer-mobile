243e3950f6000733dbef5d21bdf0d7f0ec432366
Brett Belcher <brettbelcher@Knights.ucf.edu>
2017-04-20 21:23:42 -0400

    Trying my best

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -18,7 +18,7 @@ export class Login {
 
     token: string = '';
     uniqueId: string = '';
-    url: string = '';
+    verifyUrl: string = 'http://dev-holdmybeer.azurewebsites.net/api/account/verify/';
 
     constructor(public navCtrl: NavController, private iab: InAppBrowser, private http: Http, private device: Device) {
         this.uniqueId = device.uuid;
@@ -28,11 +28,20 @@ export class Login {
 
     login() {
         const browser = this.iab.create(
-            'http://dev-holdmybeer.azurewebsites.net/api/account/login?uniqueId=' + this.uniqueId);
-
+            'http://holdmybeer.azurewebsites.net/api/account/login?uniqueId=' + this.uniqueId);
+        
         browser.on('loadstop').subscribe(data=>{
-            this.url = data.url;
-            console.log(this.url);
+            if(data.url.includes('verify'))
+            {
+                console.log(data.url);
+                console.log(this.verifyUrl + this.uniqueId);
+        
+                this.http.get(this.verifyUrl + this.uniqueId).map(res=>res.json()).subscribe(data=>{
+                    this.token = data.token;
+                });
+
+                console.log(this.token);
+            }
         });
     }
 }
